fa94beb764c07b7f3c82a92a3d64aa5a2d8284c0
Louis Mazet <louis.mazet@doctolib.com>
2024-10-10 16:07:40 +0200

    data in backend

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ app.mount("/front", StaticFiles(directory="front"), name="static")
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/test_data")
+async def test_data():
+    return {"data": [
+        ['Task', 'Hours per Day'],
+        ['Work',     11],
+        ['Eat',      2],
+        ['Commute',  2],
+        ['Play', 2],
+        ['Sleep',    7]
+    ]}
